d829d3babb51e1bcc231fcdfac1ef8e9f8dfdaa1
abdurrasyidmuhasibi <43903915+abdurrasyidmuhasibi@users.noreply.github.com>
2019-10-11 13:43:33 +0700

    upload file sepeda

---
+++ netijen/rasyid/Sepeda.java
@@ -0,0 +1,30 @@
+package JS1.praktikum;
+
+public class Sepeda {
+    private String merek;
+    private int kecepatan;
+    private int gear;
+    private String warna;
+    
+    public void setMerek(String newValue){
+        merek = newValue;
+    }
+    
+    public void gantiGear(int newValue){
+        gear = newValue;
+    }
+    
+    public void tambahKecepatan(int increment){
+        kecepatan = kecepatan + increment;
+    }
+    
+    public void rem(int decrement){
+        kecepatan = kecepatan - decrement;
+    }
+    
+    public void cetakStatus(){
+        System.out.println("Merek \t\t: " + merek);
+        System.out.println("Kecepatan \t: " + kecepatan);
+        System.out.println("Gear \t\t: " + gear);
+    }
+}
